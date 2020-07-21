01b6cf45ea0946971c97fc29788ed2b55b636953
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-20 23:16:30 -0400

    tweak... forgot to remove .data

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchShow = () => {
   return axios
     .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
-    .then(res => { return res.data })
+    .then(res => { return res })
     .catch(err => { throw err })
 };
 
